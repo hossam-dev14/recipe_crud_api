516e3c3b037f985b8b38e59a304c6d55e6609638
hossamdev <hossamtoubali@gmail.com>
2024-01-18 16:26:24 +0100

    Updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const app = require("./app");
 dotenv.config();
 
-
+// Enable CORS
 const corsOptions = {
   origin: "http://localhost:5050"
 };
@@ -17,4 +17,6 @@ app.set('port', process.env.PORT || 5050);
 const server = http.createServer(app);
 
 // Start the server
-server.listen(process.env.PORT || 5050);
+server.listen(process.env.PORT || 5050, () => {
+  console.log("Server is running on", 5050);
+});
