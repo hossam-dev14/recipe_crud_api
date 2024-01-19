facc64d68c475e607fc31b6eda5c1965601a3403
hossamdev <hossamtoubali@gmail.com>
2024-01-19 16:28:49 +0100

    Added Error Handler

--- app.js
+++ app.js
@@ -32,7 +32,23 @@ app.use(authJwt())
 app.use(bodyParser.json());
 app.use('/api/recipes', recipeRoutes);
 app.use('/api/users', userRoutes);
+app.use(errorHandler)
 
+// Error Handling function
+function errorHandler(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    // JWT authentication error
+    return res.status(401).json({message: "This data is not authorized!"});
+  }
+
+  if (err.name === "ValidationError") {
+    // Validation error
+    return res.status(401).json({message: err});
+  }
+
+  // Default to 500 server
+  return res.status(500).json(err);
+}
 
 
 
