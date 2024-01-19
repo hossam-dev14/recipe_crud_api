c457638c1cfaa6c044a415ee08c431e3756a5d88
hossamdev <hossamtoubali@gmail.com>
2024-01-19 15:11:00 +0100

    Added Signup and Login & Creating a Token

--- controllers/users.js
+++ controllers/users.js
@@ -1,60 +1,78 @@
 const User = require('../models/user.js');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// ************* Create: POST /api/users 
-const createUser = (async (req, res, next) => {
-    const { name, email} = req.body;
-  
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+// ************* SignUp *************
+const userSignup = (async (req, res) => {
+    const {firstName, lastName, email, password} = req.body;
+    const passwordhash = await bcrypt.hash(password, 10); // "hash" is asynchronous and "hashSync" is synschronous
     const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      passwordHash: bcrypt.hashSync(req.body.passwordHash, 10)
-    });
-  
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: passwordhash
+    });  
+
     // Save user to the database
     user.save()
     .then(() => {
-        if(!user)
-        return res.status(400).send('the user cannot be created!')
-
-        res.status(201).json({ 
-            message: 'User created successfully!'
-        });
+        if(!user) {
+            return res.status(400).send('the user cannot be created!');
+        }
+        res.status(201).send('User created successfully!');
     }
     ).catch(
         (error) => {
           res.status(400).json({
             error: error
         });
+
+        console.log(error);
+
     });
-    // res.send(user)
-})
-
-// // ************* Retrive : GET /api/users 
-const getUsers = (async (req, res) => {
-    const userList = await User.find().select('-passwordHash');
-    // .select('-passwordHash'); // don't display the passwordHash in this api end point
-    // .select('name phone email'); // don't show only name, phone and email in this api end point
-    if(!userList) {
-        res.status(500).json({success: false})
-    }
-    res.status(200).json({ userList })
-
 });
 
 
-// // ************* Retrive by id : GET /api/users:id
-const getUser = (async (req, res) => {
-  const user = await User.findOne({ _id: req.params.id }).select('-passwordHash');
-  if(!user) {
-      res.status(500).json({success: false})
-  }
-  res.status(200).json({ user})
+// ************* Login *************
+const userLogin = (async (req, res, next) => {
+    const { email, password } = req.body; 
+    const existingUser = await User.findOne({email:email});
+    process.env.JWT_SECRET;
+
+    if(!existingUser){
+        return res.status(401).send('The user not found');
+    }
+
+    // Comparing password 
+    const comparingBcryptPass = await bcrypt.compare(password, existingUser.password);
+    if(!comparingBcryptPass) {
+        return res.status(400).send('Invalid Credential!');
+    }
+
+    const token = jwt.sign(
+        {
+            email:existingUser.email, id:existingUser._id
+        },
+        process.env.JWT_SECRET,
+        {expiresIn : '1d'}
+    )
+        
+    res.status(200).json({
+        message: 'User is logged in',
+        token: token
+        }
+    )
+
+    // next(new CustomError(err.message, "Unable to Login"))
+
 });
 
 
 module.exports = {
-    createUser,
-    getUsers,
-    getUser
+    userSignup,
+    userLogin
 }
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 
-const {
-    createUser, 
-    getUsers, 
-    getUser} = require("../controllers/users");
+const {userSignup,userLogin } = require("../controllers/users");
 
-router.post("/",  createUser);
-router.get("/",  getUsers);
-router.get("/:id",  getUser);
+router.post("/signup",  userSignup);
+router.post("/login",  userLogin);
 
 module.exports = router;
